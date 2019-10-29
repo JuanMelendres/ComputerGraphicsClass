9e8dc91e8d4681a4b4dd9f2f6e8855360dae3640
Juan Melendres <juan.mele97@gmail.com>
2019-10-29 11:18:51 -0600

    Add Normal Shader example
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

---
+++ SecondParcial/NormalMapIntencidad/Assets/Script/SimpleRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotator: MonoBehaviour
+{
+    [SerializeField] Vector3 speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(eulers: speed * Time.deltaTime);
+    }
+}
